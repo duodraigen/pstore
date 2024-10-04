bd15a88cb0f25f845d54af625846925dda0ea193
duodraigen <youremshi@proton.me>
2024-10-04 08:55:19 -0400

    merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import { pathMap } from "./utils/pathways";
 
 import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const gProvider = createBrowserRouter(pathMap);
+const gProvider = createHashRouter(pathMap);
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<RouterProvider router={gProvider} />
-	</StrictMode>,
+	<RouterProvider router={gProvider} />,
 );
